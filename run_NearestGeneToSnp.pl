78be91c1c56ec061196b5e995e6c7a1be3785c64
Michael Maguire <mmaguire@targetvalidation.org>
2016-01-08 13:48:31 +0000

    Moved code to slurp rs IDs from file into "FileToList.pm"

--- run_NearestGeneToSnp.pl
+++ run_NearestGeneToSnp.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use v5.10;
 use NearestGeneToSnp;
+use FileToList;
 use JSON;
 
 =head
@@ -14,24 +15,13 @@ for each rs ID.
 Print the output as JSON to stdout.
 =cut
 my $rs_id_list_file = $ARGV[0];
-unless(-f $rs_id_list_file) {
-    die("No such file '$rs_id_list_file'!");
-}
-my $file_contents;
-# Slurp the file into a single string.
-{
-  local $/ = undef;
-  open my $fh, $rs_id_list_file or die "Couldn't open file: $!";
-  $file_contents = <$fh>;
-  close $fh;
-}
-# Split the slurped file into a list where each element is an rs ID.
-my @rs_ids = split(/\s+/, $file_contents);
+my $file_to_list = FileToList->new($rs_id_list_file);
+my $rs_ids = $file_to_list->get_lines_as_list();
 my $rs_id_nearest_gene_map = {};
 my $nearest_gene = NearestGeneToSnp->new();
 # Loop through each of the rs IDs and get a list of hashes with gene distance details
 # and add them to a list.
-foreach my $rs_id (@rs_ids) {
+foreach my $rs_id (@$rs_ids) {
     $rs_id_nearest_gene_map->{$rs_id} =
         $nearest_gene->get_five_prime_gene_map_list($rs_id)
 }
