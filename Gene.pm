c12c2652a9ba95f94eb8b804325f13a13b14c5b3
Michael Maguire <mmaguire@targetvalidation.org>
2016-01-12 17:06:00 +0000

    Added new method to Ensembl gene ID for reference assembly.

--- Gene.pm
+++ Gene.pm
@@ -21,20 +21,41 @@ sub new {
     my $ensembl_gene_id = shift;
     my $self = {};
     $self->{_gene} = $gene_adaptor->fetch_by_stable_id($ensembl_gene_id);
+    $self->{_ensembl_gene_id} = $ensembl_gene_id;
     bless $self, $class;
     return $self;
 }
 
-sub is_reference_ensembl_gene_idl {
+sub is_reference_ensembl_gene_id {
     my $self = shift;
-    return $self->{_gene}->slice->is_reference();
+    my $gene = shift || $self->{_gene};
+    return $gene->slice->is_reference();
 }
 # Most of the required information is contained in the hash returned by this method.
 sub get_gene_detail_map {
     my $self = shift;
-    unless($self->{_gene}) {
+    my $gene = shift || $self->{_gene};
+    unless($gene) {
 	    return {};
     }
-    return $self->{_gene}->summary_as_hash;
+    return $gene->summary_as_hash;
+}
+sub get_external_name {
+    my $self = shift;
+    return $self->{_gene}->summary_as_hash->{'Name'};
+}
+sub get_ensembl_gene_id_for_reference {
+    my $self = shift;
+    my $external_name = $self->get_external_name();
+    if($self->is_reference_ensembl_gene_id()) {
+        return $self->{_ensembl_gene_id};
+    }
+    my @genes = @{ $gene_adaptor->fetch_all_by_external_name($external_name) };
+    foreach my $gene (@genes) {
+        if($self->is_reference_ensembl_gene_id($gene)) {
+            return $self->get_gene_detail_map($gene)->{'id'};
+        }
+    }
+    return $self->{_ensembl_gene_id};
 }
 1;
