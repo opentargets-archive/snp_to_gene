2435d12ee1a08630fca9083253574c1d369a6752
Michael Maguire <mmaguire@targetvalidation.org>
2016-01-12 17:17:10 +0000

    Added documentation to the package methods.

--- Gene.pm
+++ Gene.pm
@@ -26,12 +26,17 @@ sub new {
     return $self;
 }
 
+# Returns true if the Gene object is on the reference assembly.
+# Default Gene object to check is the one created in the constructor
+# but a different Gene object can also be passed.
 sub is_reference_ensembl_gene_id {
     my $self = shift;
     my $gene = shift || $self->{_gene};
     return $gene->slice->is_reference();
 }
 # Most of the required information is contained in the hash returned by this method.
+# Default Gene object to check is the one created in the constructor
+# but a different Gene object can also be passed.
 sub get_gene_detail_map {
     my $self = shift;
     my $gene = shift || $self->{_gene};
@@ -40,10 +45,19 @@ sub get_gene_detail_map {
     }
     return $gene->summary_as_hash;
 }
+# Return the external name (HGNC-recognised symbol) for the gene object created in the constructor.
 sub get_external_name {
     my $self = shift;
     return $self->{_gene}->summary_as_hash->{'Name'};
 }
+
+# Returns an Ensembl gene ID.
+# Checks if the Ensembl gene ID used to create the Gene object is on the reference assembly. If yes, return it.
+# If no, get all the Gene objects for the external name and return the Ensembl ID for the first that is from the
+# reference assembly.
+# A small number of genes have no reference assembly, example HLA-DRB3, see:
+# http://www.ensembl.org/Homo_sapiens/Gene/Alleles?g=ENSG00000231679;r=CHR_HSCHR6_MHC_COX_CTG1:32444128-32521486
+# For such genes, simply return the Ensembl gene passed to the constructor.
 sub get_ensembl_gene_id_for_reference {
     my $self = shift;
     my $external_name = $self->get_external_name();
