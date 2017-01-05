a5730107bcc136d9bf8e4dcfcaf76c33bf351141
ChuangKee Ong <ckong@ebi.ac.uk>
2017-01-05 15:38:30 +0000

    add option to make vep endpoint rest call by region

--- snp_assignment.pl
+++ snp_assignment.pl
@@ -4,7 +4,7 @@
 
 =head1 DESCRIPTION
 
- Given a single column file with variant IDs (rs_id or clinvar_id)
+ Given a file with variant IDs and/or coordinates information
  get gene assignment and required information (SO terms etc) 
  using Ensembl Perl API and VEP REST endpoint.
 
@@ -18,7 +18,7 @@
 
  Output TSV-separated file, columns are:
 
- 1: Input rs_id
+ 1: Input rs_id/coordinates
  2: In Ensembl check (1 for yes, 0 for no)
  3: Ensembl gene stable ID
  4: Gene symbol
@@ -58,7 +58,6 @@ my $var_adaptor  = $registry->get_adaptor('human', 'variation', 'variation');
 
 my $file          = $ARGV[0];
 my $file_to_list  = FileToList->new($file);
-
 my $request_count = 0;
 my $last_request_time = Time::HiRes::time();
 
@@ -66,63 +65,65 @@ my $last_request_time = Time::HiRes::time();
 my $ids = $file_to_list->get_lines_as_list();
 
 foreach my $id (@$ids){ 
+   my $in_ensembl = '1';
 
-   my $var   = $var_adaptor->fetch_by_name($id);
-   # check variant status in ensembl
-   my $in_ensembl = defined($var) ? 1 : 0; 
+   # check rs_id status
+   if($id =~/^rs/){
+     my $var   = $var_adaptor->fetch_by_name($id);
+     $in_ensembl = defined($var) ? 1 : 0;  
+   }
 
    if($in_ensembl){
    	my $json        = _GetVepData($id);
    	my $arr_of_hash = decode_json($json);
-	my $size        = scalar(@$arr_of_hash);
 
-	# e.g. rs876660862
-	print "$id\t$in_ensembl\tVEP Error Alleles look like an insertion\n" if($size==0);
+	# e.g. rs876660862, rs869320723
+        print "$id\t$in_ensembl\tVEP Error, alleles look like an insertion\n" if(ref($arr_of_hash) ne 'ARRAY');
+
+        next if(ref($arr_of_hash) ne 'ARRAY');
 
    	foreach my $entry (@$arr_of_hash){
       		my $most_severe_consequence = $entry->{most_severe_consequence};
       		my $rs_id   = $entry->{id};
-
 		# arr_of_hash
 		my $flag    = 0;
       		my $tr_cons = $entry->{transcript_consequences};
 	
       		foreach my $entry_2 (@$tr_cons) { 
-	 		my $gene_id     = $entry_2->{gene_id};
-	 		my $gene_symbol = $entry_2->{gene_symbol};
- 	 		my $biotype     = $entry_2->{biotype};
-	 		my @terms       = @{$entry_2->{consequence_terms}};
-	 		my $terms       = join ",", @terms;
-			my $distance    = 0;
+	 	   my $gene_id     = $entry_2->{gene_id};
+	 	   my $gene_symbol = $entry_2->{gene_symbol};
+ 	 	   my $biotype     = $entry_2->{biotype};
+	 	   my @terms       = @{$entry_2->{consequence_terms}};
+	 	   my $terms       = join ",", @terms;
+		   my $distance    = 0;
 
-			# obtain gene with 'most_severe_consequence'
-                       next unless ($biotype =~/protein_coding/ || $biotype =~/miRNA/);                                                
+		   # obtain gene with 'most_severe_consequence'
+                   next unless ($biotype =~/protein_coding/ || $biotype =~/miRNA/);                                                
 
-	 		if(grep(/$most_severe_consequence/, @terms)){
-			    print "$rs_id\t$in_ensembl\t$gene_id\t$gene_symbol\t$most_severe_consequence\t$distance\n";
-	 		}
-			$flag = 1 if($biotype =~/protein_coding/);
+	 	   if(grep(/$most_severe_consequence/, @terms)){
+			print "$rs_id\t$in_ensembl\t$gene_id\t$gene_symbol\t$most_severe_consequence\t$distance\n";
+	 	   }
+		   $flag = 1 if($biotype =~/protein_coding/);
      		}
 
-#
                # e.g rs869025300
-               # revisit snp assign to miRNA gene, to get nearest protein_coding genes as well
+               # revisit snp assign to miRNA gene, 
+               # to get nearest protein_coding genes as well
                if($most_severe_consequence=~/\?/ || $most_severe_consequence=~/intergenic_variant/ || $flag==0){
-               		my $nearest_gene = _NearestGeneToSnp($rs_id);
+               	   my $nearest_gene = _NearestGeneToSnp($rs_id);
 
-                        if(scalar(@$nearest_gene) > 0){
-                               my $gene_id      = @$nearest_gene[0]->{ensembl_gene_id};
-                                my $gene_symbol  = @$nearest_gene[0]->{external_name};
-                                my $consequence  = 'nearest_gene_five_prime_end';
-                                my $distance  = @$nearest_gene[0]->{distance};
+                   if(scalar(@$nearest_gene) > 0){
+                   	my $gene_id      = @$nearest_gene[0]->{ensembl_gene_id};
+                        my $gene_symbol  = @$nearest_gene[0]->{external_name};
+                        my $consequence  = 'nearest_gene_five_prime_end';
+                        my $distance  = @$nearest_gene[0]->{distance};
                         
-                                print "$rs_id\t$in_ensembl\t$gene_id\t$gene_symbol\t$consequence\t$distance\n";
-                        } else { print "$rs_id\t$in_ensembl\tNo nearest_gene_five_prime_end found!\n"; }
+                        print "$rs_id\t$in_ensembl\t$gene_id\t$gene_symbol\t$consequence\t$distance\n";
+                   } else { print "$rs_id\t$in_ensembl\tNo nearest_gene_five_prime_end found!\n"; }
                }   
-#
   	}    
   } 
-  else { print "$id\t$in_ensembl\tVariant identifier NOT in Ensembl\n"; }
+  else { print "$id\t$in_ensembl\tVariant NOT found in Ensembl\n"; }
 
 }
 
@@ -134,10 +135,15 @@ foreach my $id (@$ids){
 sub _GetVepData {
     my ($id) = @_;
 
-    my $http     = HTTP::Tiny->new();
-    my $server   = 'https://rest.ensembl.org';
-    my $ext      = '/vep/human/id/';
+    my $http   = HTTP::Tiny->new();
+    my $server = 'https://rest.ensembl.org';
+    my $ext;
 
+    if($id =~/^rs/){
+       $ext = '/vep/human/id/';
+    }else {
+       $ext = '/vep/human/region/';
+    }
     $ext = $ext.$id."?";
 
     if($request_count == 15) { # check evey 15
@@ -170,7 +176,8 @@ sub _GetVepData {
 		return _GetVepData($id);
  	} else {
 		my ($status, $reason) = ($response->{status}, $response->{reason});
-      		die "Failed for $id! Status code: ${status}. Reason: ${reason}\n";
+		return $response->{content} if(length $response->{content});
+      	        #die "Failed for $id! Status code: ${status}. Reason: ${reason}\n";
         }
    }	
 	
