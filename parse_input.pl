925a9c62971549d08a4ce98ada5e62e91a9cd79d
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2019-05-30 16:17:45 +0100

    Extracted parsing of input file to separate script

--- parse_input.pl
+++ parse_input.pl
@@ -4,7 +4,6 @@
 =head1 DESCRIPTION
   
   Parse file supplied by EVA & GWAS to extract rsIDs and/or coordinates
-  by EVA & GWAS
 
 =pod
   
@@ -19,72 +18,31 @@ use v5.10;
 use Carp qw(confess);
 package FileToList;
 
-sub new {
-    my $class          = shift;
-    my $full_file_path = shift;
+my $file = $ARGV[0];
 
-    my $self = {};
-    $self->{full_file_path} = $full_file_path;
-    bless $self, $class;
-return $self;
-}
+my %ids;
 
-=head2 get_lines_as_list
-    
-  Ensure that the file is readable,
-  parse the file, pull required columns
-  and put them into format required by 
-  VEP endpoint. 
- 
-  * Use rs_id as first option
- 
-  Return a reference to array
+confess("File not readable!")if(! -r $file);
+open (FILE, $file) or confess("Unable to open file: $!");
 
-=cut
-sub get_lines_as_list {
-    my $self = shift;
-    my @ids;
+while(<FILE>){
+    # skipped NT expansion variants
+    next if($_=~/NT expansion/);
+    chomp $_;
+    my ($chr, $start, $end, $ref, $alt, $strand, $sv_type, $rs_id, $rcv_id, $ncbi_gene_id, $nsv_id, $misc) = split /\s+/, $_;
 
-    confess("File not readable!")if(! -r $self->{full_file_path}); 
-    open (FILE, $self->{full_file_path}) or confess("Unable to open file: $!");
-
-    while(<FILE>){
-      # skipped NT expansion variants	 
-      next if($_=~/NT expansion/);
-      chomp $_;
-      my ($chr, $start, $end, $ref, $alt, $strand, $sv_type, $rs_id, $rcv_id, $ncbi_gene_id, $nsv_id, $misc) = split /\s+/, $_;
-
-      if($rs_id =~/^rs/){
-	push @ids, $rs_id
-      }elsif($rs_id =~/\-1/){
+    if($rs_id =~/^rs/ and !exists($ids{$rs_id})){
+	    $ids{rs_id} = 1;
+    }elsif($rs_id =~/\-1/){
         my $str = '1';
         $str    = '-1' if($strand =~/\-/);
-        my $coord = $chr.":".$start."-".$end.":".$str."/".$alt;       
-        push @ids, $coord;
-      } 
-    }
-    close FILE;
+        my $coord = $chr.":".$start."-".$end.":".$str."/".$alt;
+        if(!exists($ids{coord}){
+            $ids{coord} = 1;
+        }
+      }
+}
+close FILE;
 	
-return \@ids;
-}
+print "$_\n" for keys %ids;
 
-# Does the same but deals only
-# with file with single columns of rs_id
-sub get_lines_as_list_old {
-    my $self = shift;
-
-    my $file_contents;
-    if(! -r $self->{full_file_path}) {
-        confess("File not readable!");
-    }
-    {
-        local $/ = undef;
-        open my $fh, $self->{full_file_path} or confess("Unable to open file: $!");
-        $file_contents = <$fh>;
-        close $fh;
-    }
-    # Split the slurped file into a list where each element is an rs ID.
-    my @lines = split(/\s+/, $file_contents);
-    return \@lines;
-}
-1;
