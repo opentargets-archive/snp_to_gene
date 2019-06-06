93bc35dcd28e82c827901f2f108362c94a51469a
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2019-06-06 12:01:57 +0100

    Attempt 2 to correct error with array reference

--- test.pl
+++ test.pl
@@ -13,10 +13,10 @@ my $nearest_gene = NearestGeneToSnp->new();
 my @nearest_gene = $nearest_gene->get_five_prime_nearest_gene($rs_id);
 
 if(scalar(@nearest_gene) > 0){
-                my $gene_id      = @$nearest_gene[0]->{ensembl_gene_id};
-                my $gene_symbol  = @$nearest_gene[0]->{external_name};
+                my $gene_id      = $nearest_gene[0]->{ensembl_gene_id};
+                my $gene_symbol  = $nearest_gene[0]->{external_name};
                 my $consequence  = 'nearest_gene_five_prime_end';
-                my $distance  = @$nearest_gene[0]->{distance};
+                my $distance  = $nearest_gene[0]->{distance};
 
                 print "$rs_id\t1\t$gene_id\t$gene_symbol\t$consequence\t$distance\n";
 } else { print "$rs_id\t1\tNo nearest_gene_five_prime_end found!\n"; }
